38792d1d00f3aa9011ec7e939b4af62fce90b603
igordon215 <igordon215@gmail.com>
2024-07-08 12:41:54 -0400

    working on animal

--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,27 +1,47 @@
 package com.zipcodewilmington.assessment1.part3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 2/16/18.
  */
 public class PetOwner {
+    private String name;
+    private List<Pet> pets;
     /**
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
     public PetOwner(String name, Pet... pets) {
+        this.name = name;
+        this.pets = new ArrayList<>();
+        if (pets != null) {
+            for (Pet pet : pets) {
+                addPet(pet);
+            }
+        }
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+        if (pet != null && !pets.contains(pet)) {
+            pets.add(pet);
+            pet.setOwner(this);
+        }
     }
 
     /**
      * @param pet pet to be removed from the composite collection Pets
      */
     public void removePet(Pet pet) {
-
+        if (pet != null && pets.contains(pet)) {
+            pets.remove(pet);
+            pet.setOwner(null);
+        }
     }
 
     /**
@@ -29,7 +49,7 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
-        return null;
+        return pets.contains(pet);
     }
 
     /**
@@ -47,6 +67,7 @@ public class PetOwner {
      */
     public Integer getOldestPetAge() {
         return null;
+
     }
 
 
@@ -54,27 +75,27 @@ public class PetOwner {
      * @return the sum of ages of Pet objects stored in this class divided by the number of Pet object
      */
     public Float getAveragePetAge() {
-        return null;
+     return null;
     }
 
     /**
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+        return pets.size();
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
-        return null;
+        return pets.toArray(new Pet[0]);
     }
 }
